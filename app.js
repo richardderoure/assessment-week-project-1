d1042803d9408f048d482174e04cdba79f857f80
richard <r.d.deroure@se16.qmul.ac.uk>
2020-04-01 15:37:32 +0100

    working feeling lucky button, still need to post search data somewhere

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ const express = require('express');
 const app = express();
 const port = 8080;
 const cors = require('cors');
+
 // const axios = require('axios');
 
 
 
 app.use(cors());
 app.use(express.static('public'));
-app.post('/testing', (req,res) => res.send('placeholder'));
+app.get("/", (req, res) => res.send("index"));
+app.get('/results', (req,res) => res.send('testing'));
+app.get('/testing', (req,res) => res.send('placeholder'));
 app.get('/about', (req,res) => res.send('testing'));
 app.get('/search-result', (req,res) => req.send(example));
 app.listen(port, () => console.log(`running in port ${port}`));
+
+
+
 
