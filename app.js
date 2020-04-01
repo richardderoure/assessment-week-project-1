8501768ed2ad131e44e2aac2dd08e4f0a4bb0dda
richard <r.d.deroure@se16.qmul.ac.uk>
2020-04-01 10:06:49 +0100

    committed with working api call, need to push data

--- app.js
+++ app.js
@@ -2,20 +2,14 @@ const express = require('express');
 const app = express();
 const port = 8080;
 const cors = require('cors');
+// const axios = require('axios');
 
 
 
 app.use(cors());
 app.use(express.static('public'));
-
-
-
-
-app.get('/', (req,res) => {
-    
-});
-
-
-
-
+app.post('/testing', (req,res) => res.send('placeholder'));
+app.get('/about', (req,res) => res.send('testing'));
+app.get('/search-result', (req,res) => req.send(example));
 app.listen(port, () => console.log(`running in port ${port}`));
+
--- public/script.js
+++ public/script.js
@@ -2,32 +2,33 @@
 
 $(document).ready(() => {
 
-
-
-    const createUrl = new Promise((resolve, reject) => {
-        let form = document.getElementsByClassName('form');
-        let searchInput = document.getElementById('searchInput');
-
-        $("form").submit((event) => {
-            event.preventDefault();
-            console.log(searchInput.value.split(' ').join('+'));
-            let url = 'https://www.google.com/search?q=' + searchInput.value.split(' ').join('+');
-            resolve(url);
+    const createUrl = () => {
+        return new Promise((resolve, reject) => {
+            let searchInput = document.getElementById('searchInput');
+            $("form").submit((event) => {
+                event.preventDefault();
+                console.log(searchInput.value.split(' ').join('+'));
+                let urlObject = $.get('https://www.googleapis.com/customsearch/v1?key=AIzaSyAeXkfbJjv7hCmF83KeFxTb9B5Q0cGL4Xs&cx=016496445636816056556:qvzl4ib4xtb&q=' + searchInput.value.split(' ').join('+'));
+                console.log(urlObject);
+                resolve(urlObject);
+            }); 
         });
-    });
+    };
 
     const getUrl = async () => {
         let url = await createUrl();
-        
+        console.log(url.items[3].title);
+        $.post("/testing", 'tester');    
     };
 
-
-
-    // $('#submitLink').on('click', () => {
-    //     $('#submitLink').attr('href', url);
-    // })
-
-
-
+    getUrl();
 
 });
+
+//AIzaSyD2QA22UZG7WyduOYDPgW8J1YkjnGTszzI google search api key
+
+
+
+
+
+//
